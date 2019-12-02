6e2d1a4dcc5b081ed02421be01aa77fd061e199e
krupin <evgeniy.krupin@goods.ru>
2019-12-02 14:50:59 +0300

    complete

---
+++ hw3_bench/fast.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// вам надо написать более быструю оптимальную этой функции
+func FastSearch(out io.Writer) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	r := regexp.MustCompile("@")
+	seenBrowsers := []string{}
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	lines := strings.Split(string(fileContents), "\n")
+
+	users := make([]*User, 0)
+	for _, line := range lines {
+		user := new(User)
+		// fmt.Printf("%v %v\n", err, line)
+
+		err := user.UnmarshalJSON([]byte(line))
+		//err := json.Unmarshal([]byte(line), &user)
+		if err != nil {
+			panic(err)
+		}
+		users = append(users, user)
+	}
+
+	for i, user := range users {
+
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				//if ok, err := regexp.MatchString("Android", browser); ok && err == nil {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+
+			if strings.Contains(browser, "MSIE") {
+				//if ok, err := regexp.MatchString("MSIE", browser); ok && err == nil {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := r.ReplaceAllString(user.Email, " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+}
+
+//easyjson:json
+type User struct {
+	Browsers []string `json:"browsers"`
+	Company  string   `json:"company"`
+	Country  string   `json:"country"`
+	Email    string   `json:"email"`
+	Job      string   `json:"job"`
+	Name     string   `json:"name"`
+	Phone    string   `json:"phone"`
+}
